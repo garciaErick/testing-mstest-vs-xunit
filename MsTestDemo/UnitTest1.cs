088248c533f8633bbefe32ad92ea5328af236a86
Erick Garcia <egarcia87@miners.utep.edu>
2017-04-13 23:16:56 -0600

    Finished setting up project finally D:

--- MsTestDemo/UnitTest1.cs
+++ MsTestDemo/UnitTest1.cs
@@ -7,14 +7,31 @@ namespace MsTestDemo
     [TestClass]
     public class UnitTest1
     {
+
         [TestMethod]
         public void TestMethod1()   
         {
             Blackjack bjGame = new Blackjack();
+            bjGame.beginGame();
             int noOfCards = bjGame.playerCards.Length;
             bjGame.Hit();
             int noOfCardsAfterHit = bjGame.playerCards.Length;
             Assert.AreNotEqual(noOfCards, noOfCardsAfterHit );
+            Environment.Exit(0);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            String x = "hello";
+            Blackjack bj = new Blackjack();
+            Assert.AreNotEqual(x, bj.Deal());
         }
     }
 }
--- xUnitDemo/UnitTest1.cs
+++ xUnitDemo/UnitTest1.cs
@@ -7,18 +7,7 @@ namespace MyFirstUnitTests
         [Fact]
         public void PassingTest()
         {
-            Assert.Equal(4, Add(2, 2));
-        }
-
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
-        int Add(int x, int y)
-        {
-            return x + y;
+            Assert.Equal(4, 2+2);
         }
     }
 }
